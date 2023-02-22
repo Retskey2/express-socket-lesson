148a30ece64894afde6ef25b7a6544fb9fccb5e9
RETSKEY <bashlovka96@mail.ru>
2023-02-22 19:58:58 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-const cors = require('cors')
 const express = require("express");
 const { initializeRoutes } = require("./api/routes");
-const db = require("./models");
-require("dotenv").config();
 let app = express();
+http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io'); // Add this
 
-app.use(
-  cors({
-    origin: process.env.ALLOWED_ORIGIN
-  })
-)
+const server = http.createServer(app); // Add this
+const port = process.env.PORT || 8080;
 
+app.use(cors()); // Add cors middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const port = 4000;
 app = initializeRoutes(app);
 
-  app.get("/", (req, res) => {
-    res.status(200).send({
-      success: true,
-      message: "welcome to the beginning of greatness",
-    });
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  // Add a user to a room
+  socket.on('join_room', (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
   });
+});
+
+server.listen(port, () => console.log("🚀 server is successfully running!"));
   
-  db.sequelize.sync({ force: false }).then(function () {
-    app.listen(port, function () {
-      console.log("server is successfully running!");
-    });
-  });
